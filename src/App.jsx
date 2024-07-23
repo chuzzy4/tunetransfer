ceca03ef4de69e5120a3dd5b62404b906dc1fd7b
chizu praise <chizupraise31@gmail.com>
2024-07-22 19:30:06 -0700

    changed loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { getPlaylist, getUserPlaylists } from './api/spotify';
-import axios from 'axios';
-import { saveAs } from 'file-saver';
-import Papa from 'papaparse';
-import LoginScreen from './component/LoginScreen';
-import PlaylistScreen from './component/PlaylistScreen';
-import logo from './assets/images/lg.png'
+import React, { useEffect, useState } from "react";
+import { getPlaylist, getUserPlaylists } from "./api/spotify";
+import axios from "axios";
+import { saveAs } from "file-saver";
+import Papa from "papaparse";
+import LoginScreen from "./component/LoginScreen";
+import PlaylistScreen from "./component/PlaylistScreen";
+import logo from "./assets/images/lg.png";
 
 function App() {
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
   const [playlists, setPlaylists] = useState([]);
   const [playlist, setPlaylist] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [playlistId, setPlaylistId] = useState('');
+  const [playlistId, setPlaylistId] = useState("");
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 3000);
+    }, 6000);
 
     const hash = window.location.hash;
     let token = window.localStorage.getItem("token");
 
     if (!token && hash) {
-      const tokenFragment = hash.substring(1).split("&").find(elem => elem.startsWith("access_token"));
+      const tokenFragment = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"));
       if (tokenFragment) {
         token = tokenFragment.split("=")[1];
         window.location.hash = "";
         window.localStorage.setItem("token", token);
         setToken(token);
       } else {
-        console.error('Access token not found in URL');
+        console.error("Access token not found in URL");
       }
     } else {
       setToken(token);
     }
 
     if (token) {
-      checkTokenValidity(token).then(isValid => {
+      checkTokenValidity(token).then((isValid) => {
         if (isValid) {
           fetchUserPlaylists(token);
         } else {
-          console.error('Token is invalid or expired');
+          console.error("Token is invalid or expired");
           window.localStorage.removeItem("token");
-          setToken('');
+          setToken("");
         }
       });
     }
@@ -51,10 +54,10 @@ function App() {
 
   const checkTokenValidity = async (token) => {
     try {
-      const response = await axios.get('https://api.spotify.com/v1/me', {
+      const response = await axios.get("https://api.spotify.com/v1/me", {
         headers: {
-          Authorization: `Bearer ${token}`
-        }
+          Authorization: `Bearer ${token}`,
+        },
       });
       return response.status === 200;
     } catch (error) {
@@ -70,69 +73,72 @@ function App() {
       const data = await getUserPlaylists(token);
       setPlaylists(data.items);
     } catch (error) {
-      console.error('Error fetching user playlists:', error);
+      console.error("Error fetching user playlists:", error);
     }
   };
 
   const handleFetchPlaylist = async () => {
     if (playlistId) {
-      setLoading(true); 
+      setLoading(true);
       try {
         const playlist = await getPlaylist(playlistId, token);
         setPlaylist(playlist);
       } catch (error) {
-        console.error('Error fetching playlist:', error);
-        alert('Invalid Playlist ID', error)
+        console.error("Error fetching playlist:", error);
+        alert("Invalid Playlist ID", error);
         if (error.response && error.response.status === 401) {
           window.localStorage.removeItem("token");
-          setToken('');
+          setToken("");
         }
       }
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
   const handleExport = () => {
     if (playlist) {
-      const data = playlist.tracks.items.map(item => ({
+      const data = playlist.tracks.items.map((item) => ({
         name: item.track.name,
-        artist: item.track.artists.map(artist => artist.name).join(', '),
-        album: item.track.album.name
+        artist: item.track.artists.map((artist) => artist.name).join(", "),
+        album: item.track.album.name,
       }));
 
       const csv = Papa.unparse(data);
-      const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+      const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
       saveAs(blob, `${playlist.name}.csv`);
     }
   };
 
   const handleExportAll = async () => {
-    setLoading(true); 
+    setLoading(true);
     for (const pl of playlists) {
       try {
         const playlist = await getPlaylist(pl.id, token);
-        const data = playlist.tracks.items.map(item => ({
+        const data = playlist.tracks.items.map((item) => ({
           name: item.track.name,
-          artist: item.track.artists.map(artist => artist.name).join(', '),
-          album: item.track.album.name
+          artist: item.track.artists.map((artist) => artist.name).join(", "),
+          album: item.track.album.name,
         }));
 
         const csv = Papa.unparse(data);
-        const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+        const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
         saveAs(blob, `${playlist.name}.csv`);
       } catch (error) {
-        console.error('Error exporting playlist:', error);
+        console.error("Error exporting playlist:", error);
       }
     }
-    setLoading(false); 
+    setLoading(false);
   };
 
   if (loading) {
     return (
       <div className="min-h-screen p-4 bg-slate-800 dark:bg-zinc-900 flex items-center justify-center">
         <div className="text-center">
-        <img src={logo} alt="watermark" className="max-w-full max-h-full animate__animated animate__flash animate__slow" />
-    
+          <img
+            src={logo}
+            alt="watermark"
+            className="w-32 h-32 animate__animated animate__flash animate__slow"
+          />
         </div>
       </div>
     );
--- src/component/PlaylistScreen.jsx
+++ src/component/PlaylistScreen.jsx
@@ -1,85 +1,136 @@
-import React from 'react';
-import logo from '../assets/images/lg.png'
-
+import React from "react";
+import logo from "../assets/images/lg.png";
 
 function PlaylistScreen({
-    playlists,
-    playlist,
-    token,
-    playlistId,
-    setPlaylistId,
-    handleFetchPlaylist,
-    handleExport,
-    handleExportAll,
+  playlists,
+  playlist,
+  token,
+  playlistId,
+  setPlaylistId,
+  handleFetchPlaylist,
+  handleExport,
+  handleExportAll,
 }) {
-    return (
-        <div className="min-h-screen bg-slate-800 dark:bg-zinc-900 relative">
-            <div className="dark:bg-zinc-800 rounded-md relative z-10">
-                <div className="flex flex-col md:flex-row justify-between items-center mb-4 sticky top-0 z-50 bg-white md:shadow-xl shadow-md p-5 w-full">
-                    <h1 className="text-xl font-bold text-zinc-900 dark:text-zinc-100 mb-4 md:mb-0">Tune Transfer</h1>
-                    <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
-                        <input
-                            type="text"
-                            placeholder="Enter Playlist ID"
-                            className="p-2 border rounded-md dark:bg-zinc-700 dark:border-zinc-600 dark:text-zinc-300"
-                            value={playlistId}
-                            onChange={(e) => setPlaylistId(e.target.value)}
-                        />
-                        <button onClick={handleFetchPlaylist} className="bg-black text-white px-4 py-2 rounded-md  items-center">+</button>
-                        <button onClick={handleExport} className="bg-black text-white px-4 py-2 rounded-md  items-center">
-                            Export as CSV
-                        </button>
-                        <a href="https://drive.google.com/file/d/1YmTZ9VIAZxjr2GN4V7rBkOuktd6WuLUe/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-                            <button onClick={handleExportAll} className="bg-black text-white px-4 py-2 rounded-md  items-center">
-                                Docs
-                            </button>
-                        </a>
-                    </div>
-                </div>
-
-                <div className="space-y-4">
-                    {playlists.map((pl, index) => (
-                        <div key={index} className="bg-white dark:bg-zinc-800 p-4 rounded-md shadow-sm flex items-start space-x-4 border border-zinc-200 dark:border-zinc-700">
-                            <div>
-                                <img aria-hidden="true" alt="music-note" src={pl.images.length > 0 ? pl.images[0].url : 'https://placehold.co/32'} className="w-8 h-8" />
-                            </div>
-                            <div className="flex-grow">
-                                <h2 className="text-xl font-semibold text-zinc-900 dark:text-zinc-100">{pl.name}</h2>
-                                <p className="text-zinc-600 dark:text-zinc-400">{pl.description || 'No description'}</p>
-                            </div>
-                            <span className="text-zinc-500 dark:text-zinc-400">{pl.id}</span>
-                        </div>
-                    ))}
-                </div>
-
-                {playlist && (
-                    <div className="mt-4 space-y-4">
-                        <div className='flex flex-col gap-5 m-5'>
-                            {playlist.tracks.items.map((item, index) => (
-                                <div className="bg-white dark:bg-zinc-800 p-4 rounded-md shadow-sm flex items-start space-x-4 border border-zinc-200 dark:border-zinc-700 animate__animated animate__fadeIn" key={index}>
-                                    <div>
-                                        <img aria-hidden="true" alt="album-cover" src={item.track.album.images.length > 0 ? item.track.album.images[0].url : 'https://placehold.co/32'} className="w-16 h-16" />
-                                    </div>
-                                    <div className="flex-grow">
-                                        <h2 className="md:text-xl text-sm font-semibold text-zinc-900 dark:text-zinc-100">{item.track.name}</h2>
-                                        <p className="text-zinc-600 dark:text-zinc-400 text-xs">{item.track.artists.map(artist => artist.name).join(', ')}</p>
-                                    </div>
-                                    <p className="text-purple-900 dark:text-zinc-400 text-xs">{item.track.album.release_date}</p>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                )}
-            </div>
-
-
-            {!playlist && (
-                <div className="absolute inset-0 flex justify-center items-center opacity-30">
-                    <img src={logo} alt="watermark" className="max-w-full max-h-full" />
-                </div>
-            )}
+  return (
+    <div className="min-h-screen bg-slate-800 dark:bg-zinc-900 relative">
+      <div className="dark:bg-zinc-800 rounded-md relative z-10">
+        <div className="flex flex-col md:flex-row justify-between items-center mb-4 sticky top-0 z-50 bg-white md:shadow-xl shadow-md p-5 w-full">
+          <h1 className="text-xl font-bold text-zinc-900 dark:text-zinc-100 mb-4 md:mb-0">
+            Tune Transfer
+          </h1>
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2">
+            <input
+              type="text"
+              placeholder="Enter Playlist ID"
+              className="p-2 border rounded-md dark:bg-zinc-700 dark:border-zinc-600 dark:text-zinc-300"
+              value={playlistId}
+              onChange={(e) => setPlaylistId(e.target.value)}
+            />
+            <button
+              onClick={handleFetchPlaylist}
+              className="bg-black text-white px-4 py-2 rounded-md  items-center"
+            >
+              +
+            </button>
+            <button
+              onClick={handleExport}
+              className="bg-black text-white px-4 py-2 rounded-md  items-center"
+            >
+              Export as CSV
+            </button>
+            <a
+              href="https://drive.google.com/file/d/1YmTZ9VIAZxjr2GN4V7rBkOuktd6WuLUe/view?usp=sharing"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button
+                onClick={handleExportAll}
+                className="bg-black text-white px-4 py-2 rounded-md  items-center"
+              >
+                Docs
+              </button>
+            </a>
+          </div>
         </div>
-    );
+
+        <div className="space-y-4 ">
+          {playlists.map((pl, index) => (
+            <div
+              key={index}
+              className="bg-white dark:bg-zinc-800 p-4 rounded-md shadow-sm flex items-start space-x-4 border border-zinc-200 dark:border-zinc-700 mx-5"
+            >
+              <div>
+                <img
+                  aria-hidden="true"
+                  alt="music-note"
+                  src={
+                    pl.images.length > 0
+                      ? pl.images[0].url
+                      : "https://placehold.co/32"
+                  }
+                  className="w-8 h-8"
+                />
+              </div>
+              <div className="flex-grow">
+                <h2 className="text-xl font-semibold text-zinc-900 dark:text-zinc-100">
+                  {pl.name}
+                </h2>
+                <p className="text-zinc-600 dark:text-zinc-400">
+                  {pl.description || "No description"}
+                </p>
+              </div>
+              <span className="text-zinc-500 dark:text-zinc-400">{pl.id}</span>
+            </div>
+          ))}
+        </div>
+
+        {playlist && (
+          <div className="mt-4 space-y-4">
+            <div className="flex flex-col gap-5 m-5">
+              {playlist.tracks.items.map((item, index) => (
+                <div
+                  className="bg-white dark:bg-zinc-800 p-4 rounded-md shadow-sm flex items-start space-x-4 border border-zinc-200 dark:border-zinc-700 animate__animated animate__fadeIn"
+                  key={index}
+                >
+                  <div>
+                    <img
+                      aria-hidden="true"
+                      alt="album-cover"
+                      src={
+                        item.track.album.images.length > 0
+                          ? item.track.album.images[0].url
+                          : "https://placehold.co/32"
+                      }
+                      className="w-16 h-16"
+                    />
+                  </div>
+                  <div className="flex-grow">
+                    <h2 className="md:text-xl text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+                      {item.track.name}
+                    </h2>
+                    <p className="text-zinc-600 dark:text-zinc-400 text-xs">
+                      {item.track.artists
+                        .map((artist) => artist.name)
+                        .join(", ")}
+                    </p>
+                  </div>
+                  <p className="text-purple-900 dark:text-zinc-400 text-xs">
+                    {item.track.album.release_date}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+
+      {!playlist && (
+        <div className="absolute inset-0 flex justify-center items-center opacity-30">
+          <img src={logo} alt="watermark" className="max-w-full max-h-full" />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default PlaylistScreen;
