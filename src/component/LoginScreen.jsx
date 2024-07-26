ba6ce197d67b876d63cb5cb409c3eca15f9bcb7b
Your Name <chizupraise31@gmail.com>
2024-07-26 05:06:06 -0700

    changed font

--- src/component/LoginScreen.jsx
+++ src/component/LoginScreen.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { getLoginUrl } from '../api/spotify';
+import React from "react";
+import { getLoginUrl } from "../api/spotify";
 
 function LoginScreen() {
   return (
-
     <div className="fixed inset-0 flex items-center justify-center bg-gradient-to-r from-green-400 to-green-600">
       <div className="relative m-6 p-6 bg-white rounded-lg shadow-md max-w-md w-full">
         <button className="absolute top-2 right-2 text-zinc-500 focus:outline-none">
@@ -12,35 +11,41 @@ function LoginScreen() {
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg">
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"></path>
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
           </svg>
         </button>
-        <h2 className="text-2xl font-bold mb-2 text-center">Tune Transfer</h2>
+        <h2 className="text-2xl font-bold mb-2 text-center font-mono">
+          Tune Transfer
+        </h2>
         <p className="mb-4 text-center text-zinc-600">
           Backup your Spotify playlists with ease. Login to get started.
         </p>
         <div className="flex justify-center">
           <a href={getLoginUrl()}>
-          <button className="bg-green-500 text-white py-2 px-4 rounded-full flex items-center">
-            <svg
-              className="w-5 h-5 mr-2"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 13l4 4L19 7"></path>
-            </svg>
-            Login to Spotify
-          </button>
+            <button className="bg-green-500 text-white py-2 px-4 rounded-full flex items-center">
+              <svg
+                className="w-5 h-5 mr-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                ></path>
+              </svg>
+              Login to Spotify
+            </button>
           </a>
         </div>
       </div>
